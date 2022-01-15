f4c6bd27c826360965dd800dfcab605333adea44
elleon003 <ngrandison@ymail.com>
2022-01-15 16:04:43 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'blue': '#343276',
+      'gold': '#BEA187',
+      'light': '#F1F1F9',
+      'dark': '#0D0C1D',
+      'grey': '#7F7F88',
+      'white': '#ffffff',
+      'black': '#000000'
+    },
+    fontFamily: {
+      'body': ['Montserrat', 'ui-sans-serif'],
+      'sans': ['Montserrat', 'ui-sans-serif'],
+      'display': ['PermanentMarker', 'ui-serif']
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
